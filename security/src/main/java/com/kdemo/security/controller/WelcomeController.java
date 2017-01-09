de3636a5944d22acb33c0365f59f29001084b6f1
Mr.SAY SEAK LENG <Mr.SAY SEAK LENG@192.168.1.104>
2017-01-09 22:40:57 +0700

    - Add persistent token remember-me

--- security/src/main/java/com/kdemo/security/controller/WelcomeController.java
+++ security/src/main/java/com/kdemo/security/controller/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.kdemo.security.controller;
 
+import java.security.Principal;
 import java.util.Date;
 
 import org.springframework.stereotype.Controller;
@@ -10,9 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WelcomeController {
 	
 	@GetMapping("/")
-	public String getWelcomePage(Model model) {
+	public String getWelcomePage(Model model, Principal principal) {
 		
-		model.addAttribute("name", "Lan");
+		model.addAttribute("name", principal.getName());
 		model.addAttribute("systemDateTime", new Date());
 		
 		return "welcome";
